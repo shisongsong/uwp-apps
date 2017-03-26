f65ef3a016a35b32b4260aa969d4bc2f0b1fca30
石嵩 <shi1400875096@outlook.com>
2017-03-26 17:03:45 +0800

    add blank_app

---
+++ blank_app/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+public class Library
+{
+    public bool rotating = false;
+    private Storyboard rotation = new Storyboard();
+
+    public string LoadSetting(string key)
+    {
+        if (ApplicationData.Current.LocalSettings.Values[key] != null)
+        {
+            return ApplicationData.Current.LocalSettings.Values[key].ToString();
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+
+    /// <summary>
+    /// 保存设置
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
+    public void SaveSetting(string key, string value)
+    {
+        ApplicationData.Current.LocalSettings.Values[key] = value;
+    }
+
+    public void Rotate(string axis, ref Image target)
+    {
+        if (rotating)
+        {
+            rotation.Stop();
+            rotating = false;
+        }
+        else
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0.0;
+            animation.To = 360.0;
+            animation.BeginTime = TimeSpan.FromSeconds(1);
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            Storyboard.SetTarget(animation, target);
+            // 字符串里的内容十分容易出错，一定要谨慎输入，或者直接复制粘贴，比较保险一点
+            Storyboard.SetTargetProperty(animation, "(UIElement.Projection).(PlaneProjection.Rotation" + axis + ")");
+            rotation.Children.Clear();
+            rotation.Children.Add(animation);
+            rotation.Begin();
+            rotating = true;
+        }
+    }
+}
---
+++ blank_app/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace blank_app
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public Library Library = new Library();
+
+        //private void New_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Email.Text = string.Empty;
+        //    Website.Text = string.Empty;
+        //    Telephone.Text = string.Empty;
+        //}
+
+        //private void Open_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Email.Text = Library.LoadSetting("Email");
+        //    Website.Text = Library.LoadSetting("Website");
+        //    Telephone.Text = Library.LoadSetting("Telephone");
+        //}
+
+        //private void Save_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Library.SaveSetting("Email", Email.Text);
+        //    Library.SaveSetting("Website", Website.Text);
+        //    Library.SaveSetting("Telephone", Telephone.Text);
+        //}
+
+        private void Pitch_Click(object sender, RoutedEventArgs e)
+        {
+            Library.Rotate("X", ref Display);
+        }
+
+        private void Roll_Click(object sender, RoutedEventArgs e)
+        {
+            Library.Rotate("Y", ref Display);
+        }
+
+        private void Yaw_Click(object sender, RoutedEventArgs e)
+        {
+            Library.Rotate("Z", ref Display);
+        }
+
+        private void Go_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter)
+            {
+                Display.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(Value.Text));
+            }
+        }
+    }
+}
